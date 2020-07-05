d167ec7d2504f59349dcbe73abb649864f2bb00a
poori99988 <66064058+poori99988@users.noreply.github.com>
2020-07-06 00:25:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+﻿const Discord = require("discord.js")
+const bot = new Discord.Client();
+const path = require('path');
+global.config = require("./config/config.js")
+global.prefix = `${config.prefix}`;
+
+//req zone
+const fs = require("fs");
+const talkedRecently = new Set();
+
+
+//dont touch it!
+global.appRoot = path.resolve(__dirname);
+bot.commands = new Discord.Collection()
+bot.aliases = new Discord.Collection()
+
+
+//read command!
+fs.readdir(config.pathcmd, (err, files) => {
+	if (err)
+		console.error(err)
+	global.jsfiles = files.filter(f => f.split(".").pop() === "js");
+	global.jsfiles = jsfiles.filter(f => f[0]!="-");
+	if (jsfiles.length <= 0) {
+		return
+	}
+	jsfiles.forEach(f => {
+		let props = require(`${config.pathcmd}${f}`)
+		props.fileName = f
+		bot.commands.set(props.help.name, props)
+		props.conf.aliases.forEach(alias => {
+			bot.aliases.set(alias, props.help.name)
+		})
+	})
+})
+
+require('./config/event')(bot);
+bot.login(config.token); 
---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+:loop
+cls
+color 0e
+echo *-*-*-*-*-*-*-*-*-*-*-*
+echo  Bot By Golden
+echo *-*-*-*-*-*-*-*-*-*-*-*
+node index
+goto loop
